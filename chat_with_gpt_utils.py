48d5d3d9ecc223765dea3c7e0a76c493ba2d92f0
mhd almasri <a3sthet1k@proton.me>
2024-12-22 17:23:41 +0100

    new app code

--- chat_with_gpt_utils.py
+++ chat_with_gpt_utils.py
@@ -1,6 +1,47 @@
+import os
+import re
+import sys
+import tkinter as tk
+from tkinter import *
 
-var1 = True
 
-str(var1)
 
-print(str(var1), "\t", type(str(var1)))
+R = {"path": [r"C:\Users\mhd\Downloads\imagepython", r"C:\Users\mhd\Downloads\sound1", r"C:\Users\mhd\Downloads\rest"]}
+
+
+windows = tk.Tk()
+windows.title("file organiser")
+windows.geometry("1000x600")
+windows.config(bg=None , cursor="arrow" , relief="sunken" , borderwidth=10 ,)
+windows.resizable(width=False, height=False)
+
+windows.mainloop()
+
+
+
+
+
+while True:
+    def imagefile():
+        if not os.path.exists(R ["path"][0]):
+            os.mkdir(R ["path"][0])
+        else:
+            print(f"folder already exist:", R ["path"][0])
+
+
+    def soundfile():
+        if not os.path.exists(R ["path"][1]):
+            os.mkdir(R ["path"][1])
+        else:
+            print(f"folder already exist:", R ["path"][1])
+
+
+
+    def restfile():
+        if not os.path.exists(R ["path"][2]):
+            os.mkdir(R ["path"][2] )
+        else:
+            print(f"folder already exist:", R ["path"][2])
+
+    break
+
