83ce1af885a92052e4c6eae0ee8d949c387850f2
mhd almasri <lomolopo@outlook.de>
2025-03-12 19:16:59 +0100

    fg

---
+++ name/test wift.py
@@ -0,0 +1,9 @@
+import wifite
+from wifite import *
+
+
+def test_wift():
+    assert wifite.wift() == "wift"
+    if password in rockyou.txt
+        get user identificaton
+        get user password
